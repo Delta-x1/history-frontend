db0b1553fa68b11da819d48aeb943004363c826a
ntlam <ntlamcntt1995@gmail.com>
2026-07-31 18:34:25 +0700

    update 1

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-node"; // Chuyển từ adapter-auto sang adapter-node
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
+  compilerOptions: {
+    runes: ({ filename }) =>
+      filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
+  },
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // Tùy chọn cho adapter-node
+      out: "build",
+      precompress: false,
+      envPrefix: "",
+    }),
   },
 };
 
